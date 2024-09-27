bdb4a5c21aeae93a8594e3e6e037c97dd43be9d7
S184929 <sigurd.staloy@gmail.com>
2024-09-27 11:59:50 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -5,17 +5,37 @@ public class Tabeller {
 		int[] tabell = { 1, 2, 3, 4, 5 };
 		int[] tabell2 = { 42, 67, 89 };
 		int[] tabell3 = { 10, 20, 30 };
+		int[] tabell4 = { 5, 10, 15 };
+		int[] tabell5= {1,3,5,7,10};
+		int[] tabell6 = {2,4,6,8,10};
+		
 		
 		skrivUt(tabell);
 		tilStreng(tabell2);
 		
 		
-		
 		int totalsum =summer(tabell3);
-		
 		System.out.println("summen av tabell3 er: " + totalsum);
+		
+		//Her kan du redigere variabelen for å sjekke om den er i tabell4
+		int tall4=5;
+		finnesTall(tabell4, tall4);
+			
+		int tall5=7;
+		int posisjon =posisjonTall(tabell5, tall5);
+		
+		if (posisjon !=-1) {
+			System.out.println("Tallet " + tall5 + " finnes i posisjonen " + posisjon + " i tabell5");
+		} else {
+			System.out.println("Tallet " + tall5 + " finnes ikke i tabellen");
+			
+		}
+		//ikke fått denne til å fungere
+		int[] motsatt = reverser(tabell6);
+		System.out.println(motsatt);
+		
+		}
 
-	}
 	// a)
 
 	public static void skrivUt(int[] tabell) {
@@ -68,24 +88,47 @@ public class Tabeller {
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
+		for (int i =0; i<tabell.length; i++) {
+			if (tabell[i]==tall) {
+				System.out.println("Tallet " + tall + " er i tabellen");
+				return true;
+			}
+			
+		}
+		System.out.println("Tallet er ikke i tabellen");
+		return false;
+		
 
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+		for (int i =0; i<tabell.length; i++) {
+			if (tabell[i]==tall) {
+				return i;
+				
+			}
+		}
+		return -1;
 	}
 
-	// f)
+	// f) har ikke fått denne til å fungere
 	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden reverser ikke implementert");
+		int[] reversert = new int[tabell.length];
+		for (int i =tabell.length; i>=0; i--) {
+			System.out.println(tabell[i]);
+			if (i>0) {
+				System.out.println(", ");
+			reversert[i]=(tabell.length-1-i);	
+			}
+		System.out.println("]");
+		}
+		
+		
+		
+		return reservert;
+		
+		
 	}
 
 	// g)
